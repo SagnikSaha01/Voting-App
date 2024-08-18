a55ddd97e4107559cc448c0e4f10f193ad310c60
SagnikSaha01 <sahasagnik999@gmail.com>
2024-08-17 21:27:42 -0400

    Restructure of files

---
+++ server/routes/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require("body-parser");
+
+const handle = require('C://Users//Swagnik//Desktop//Voting-App//server//handlers');
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.json({hello: 'world'}));
+
+app.use(handle.notFound);
+app.use(handle.errors);
+
+app.listen(port, console.log('Server started on port ' + port));
